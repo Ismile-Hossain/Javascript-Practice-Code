6299c3d27385a573dcd7cda13ea4bb1f00962af5
unknown <mdismilehossain2111@gmail.com>
2022-07-20 15:13:33 +0600

    index&Include&LastIndexFind

---
+++ Array/findArrElement.js
@@ -0,0 +1,6 @@
+const arr = [1,2,3,4,5,6,7,4,8]; // not reassignable
+console.log(arr.includes(4));
+console.log(arr.includes(4,5));
+console.log(arr.indexOf(4));
+console.log(arr.indexOf(9));
+console.log(arr.lastIndexOf(4));
