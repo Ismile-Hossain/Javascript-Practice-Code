2173bd8cad3eeafdaadff7465ffa902e011fca26
unknown <mdismilehossain2111@gmail.com>
2022-07-20 15:20:46 +0600

    arrJoinWtSpecialChar&SplitStrToArr

---
+++ Array/arrJoin.js
@@ -0,0 +1,8 @@
+//arr to string using join
+let arr = [1,2,3,4];
+let joinArr = arr.join("-");
+console.log(joinArr);
+
+let str = "I am Ismile Hossain";
+let strToArr = str.split(" ");
+console.log(strToArr);
