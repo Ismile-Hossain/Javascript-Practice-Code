0367314a45738ab4ee889756f4502788d4db38fb
unknown <mdismilehossain2111@gmail.com>
2022-07-31 13:35:13 +0600

    removeStrFromStrtEnd

---
+++ Es6Es7Es8Features/trimStartEnd.js
@@ -0,0 +1,13 @@
+let str = "  mid  "
+console.log(str);
+console.log(str.length);
+str = str.trim();
+console.log(str.length);
+
+let strr = "  mid  ";
+console.log(strr);
+console.log(strr.length);
+strr = strr.trimStart();
+console.log(strr.length);
+strr = strr.trimEnd();
+console.log(strr.length);
