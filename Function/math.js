63f042e60ae6cfa70316090af61f0555fa5a1eeb
unknown <mdismilehossain2111@gmail.com>
2022-07-31 12:24:20 +0600

    exportFunGlobally

---
+++ Function/math.js
@@ -0,0 +1,5 @@
+//const addition = (num1,num2) => num1+num2;
+export const addition = (num1,num2) => num1+num2;
+export const sub = (num1,num2) => num1-num2;
+
+export default addition;//for single function call
