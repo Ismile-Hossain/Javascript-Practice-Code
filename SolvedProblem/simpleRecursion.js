0b8ba36bdb453d04135316d74e2f831d51917176
unknown <mdismilehossain2111@gmail.com>
2022-08-12 22:09:26 +0600

    recursionExample

--- SolvedProblem/simpleRecursion.js
+++ SolvedProblem/simpleRecursion.js
@@ -1,6 +1,7 @@
 function number(num){
- if(num == 10) return;
-   console.log(num);
+ if(num > 10) return;
+   //console.log(num); print 1 to 10 like queue
    number(num+1);
+   console.log(num); //print 10 to 1 like stack
 }
 number(1);
