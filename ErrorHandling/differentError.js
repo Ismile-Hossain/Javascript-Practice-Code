d3b938de6027d385ccd889cc7632abfaec39bdd5
unknown <mdismilehossain2111@gmail.com>
2022-08-13 13:06:25 +0600

    error handling

---
+++ ErrorHandling/differentError.js
@@ -0,0 +1,5 @@
+let va ri able = "Ismile"; //this gives syntax error
+let bool = true;
+console.log(bool.toUpperCase()); //this gives type error
+let pi = 3.141592654;
+console.log(pi.toFixed(101)); //this gives range error
