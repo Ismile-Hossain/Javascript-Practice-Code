e9e79f437880d541a4eed3620835cb0717c247eb
unknown <mdismilehossain2111@gmail.com>
2022-10-29 23:18:45 +0600

    domObjs

---
+++ Dom/index.js
@@ -0,0 +1,31 @@
+// I wish you good luck and happy coding 🥰🤠🥳🥳💯💯
+console.log(document);
+console.log(document.all);
+console.log(document.all[1]);
+console.log(document.head);
+console.log(document.all[7]);
+console.log(document.body);
+console.log(document.all.length);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.doctype);
+console.log(document.charset);
+console.log(document.contentType);
+console.log(document.body.className);
+console.log(document.body.classList);
+console.log(document.forms);
+console.log(document.forms[0]);
+console.log(document.forms[0].id);
+console.log(document.forms[0].className);
+console.log(document.forms[0].action);
+console.log(document.forms[0].method);
+console.log(document.links);
+console.log(document.scripts);
+console.log(document.scripts[3]);
+console.log(document.scripts[3].getAttribute('src'));
+
+let scripts =  Array.from(document.scripts);
+
+scripts.forEach((script)=>console.log(script));
+
+scripts.forEach((script)=>console.log(script.getAttribute('src')));
