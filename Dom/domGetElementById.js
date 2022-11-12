96c935f78dc1518cb1f4f8afe57bc9bd97111dcb
unknown <mdismilehossain2111@gmail.com>
2022-11-12 14:24:46 +0600

    domGetElementById

---
+++ Dom/domGetElementById.js
@@ -0,0 +1,12 @@
+let header = document.getElementById('add-post-title-id');
+console.log(header);
+console.log(header.id);
+console.log(header.className);
+console.log(header.classList);
+header.style.backgroundColor = "red";
+header.style.color = "white";
+header.style.padding = "15px";
+//header.style.display = 'none';
+header.textContent = "Hi Ismile!!";
+header.innerText = "Hi Shuvo!!";
+header.innerHTML = '<span style = "color:black">Hi Mikhel!! </span>';
