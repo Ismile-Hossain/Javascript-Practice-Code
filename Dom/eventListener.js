6080590f1564d421ae001ed8c1f8c16d7f8d6d2d
unknown <mdismilehossain2111@gmail.com>
2022-11-12 22:27:14 +0600

    eventListeners

---
+++ Dom/eventListener.js
@@ -0,0 +1,29 @@
+const clrBtn = document.querySelector('.clear-post');
+
+// clrBtn.addEventListener('click',function(){
+// console.log("Click!!");   
+// });
+
+// clrBtn.addEventListener('dblclick',function(){
+// console.log("Double click!!");   
+// });
+
+// clrBtn.addEventListener('mousedown',function(){
+// console.log("Mousedown!!");   
+// });
+
+// clrBtn.addEventListener('mouseup',function(){
+// console.log("Mouse Up!!");   
+// });
+
+// clrBtn.addEventListener('mouseenter',function(){
+// console.log("Mouse Enter!!");   
+// });
+
+// clrBtn.addEventListener('mouseleave',function(){
+// console.log("Mouse Leave!!");   
+// });
+
+clrBtn.addEventListener('mousemove',function(){
+console.log("Mouse Leave!!");   
+});
